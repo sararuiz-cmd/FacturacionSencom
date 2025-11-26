18d25f416f14ed38b838414756ccb29d6fdca798
Sara Ruiz <svruiz@uamv.edu.ni>
2025-11-26 09:10:09 -0600

    Quite un boton extra

--- UI/FrmPrediccionIA.Designer.cs
+++ UI/FrmPrediccionIA.Designer.cs
@@ -38,7 +38,6 @@ namespace Proyect_Sencom_Form.UI
             this.panelHeader = new System.Windows.Forms.Panel();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.panelContenido = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.panelHeader.SuspendLayout();
             this.panelContenido.SuspendLayout();
             this.SuspendLayout();
@@ -176,25 +175,10 @@ namespace Proyect_Sencom_Form.UI
             this.panelContenido.Size = new System.Drawing.Size(380, 200);
             this.panelContenido.TabIndex = 0;
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.DimGray;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(20, 268);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 30);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Atrás";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.lblMensaje_Click);
-            // 
             // FrmPrediccionIA
             // 
             this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(420, 310);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panelContenido);
             this.Controls.Add(this.panelHeader);
             this.Controls.Add(this.lblMensaje);
@@ -211,7 +195,5 @@ namespace Proyect_Sencom_Form.UI
             this.ResumeLayout(false);
 
         }
-
-        private Button button1;
     }
 }
